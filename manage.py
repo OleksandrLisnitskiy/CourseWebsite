b308b915dfab42f4aa24d082a87b7c7e53cefa44
Oleksandr Lisnytskyi <oleksandrl1009@gmail.com>
2025-10-02 14:56:42 -0600

    chore(formating): black reformated

--- manage.py
+++ manage.py
@@ -9,7 +9,6 @@ def main():
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_settings.settings")
 
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
