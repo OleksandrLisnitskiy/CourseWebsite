fb8469ab4dc2cc631dc0412e6b1c0456800bf00b
Oleksandr Lisnytskyi <oleksandrl1009@gmail.com>
2025-10-02 14:33:48 -0600

    chore(formating): refactored code for flake8

--- project_settings/settings.py
+++ project_settings/settings.py
@@ -63,15 +63,17 @@ ROOT_URLCONF = "project_settings.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-      ],
+
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+
         },
     },
 ]
